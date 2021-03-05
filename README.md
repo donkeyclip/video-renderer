e0b9783331722ff9bf6b161358a3170350e993d3
Theofilos Papadopoulos <theofilos.p@kissmybutton.gr>
2021-03-06 01:54:46 +0200

    finish workers implementation

--- README.md
+++ README.md
@@ -9,16 +9,18 @@ node node_modules/dclip-video-renderer -- -i <clip_uid>;
 where in `<clip_uid>` add the dockey clip uid you want to convert.
 
 # Options
-| argument | value type | description |
-| --- | --- | ---
-| -i | string | The unique donkey clip id as an Input |
-| -o | string | The Output file path |
-| -r | integer | The frameRate of the output video |
-| -q | integer | The Quality of the video |
-| -w | integer | The Width of the video |
-| -h | integer | The Height of the video |
-| -s | integer | The Start milisecond of the donkey clip |
-| -e | integer | The end milisecond of the donkey clip |
+| argument | type|default | description |
+| --- | --- | --- | ---
+| -i | string | null | The unique donkey clip id as an Input |
+| -o | string [*.mp4] | ./out.mp4  | The Output file path |
+| -r | integer | 24  | FrameRate of the output video |
+| -q | integer [0-100]| 80  | Quality of the video |
+| -w | integer | 720 | Width of the video |
+| -h | integer | 640|Height of the video |
+| -s | integer | 0|Start milisecond of the donkey clip |
+| -e | integer | <clip.duration>|End milisecond of the donkey clip |
+| -c | integer | total cpus / 2 |Number of cpus to use |
+| -env | integer ["local","staging","production"] | production | Number of cpus to use |
 
 #### Full Example
-`node node_modules/dclip-video-renderer -- -i 7179a8c1-2DC3 -o ./dclip.mp4 -r 24 -q 80 -w 720 -h 640 -s 1000 -e 5000;`
+`node node_modules/dclip-video-renderer -- -i 7179a8c1-2DC3 -o ./dclip.mp4 -r 24 -q 80 -w 720 -h 640 -s 1000 -e 5000 -c 4;`
--- index.js
+++ index.js
@@ -1,189 +1,42 @@
-(async function () {
-  const {
-    Worker,
-    isMainThread,
-    parentPort,
-    workerData,
-  } = require("worker_threads");
+"use strict";
+const { isMainThread, parentPort, workerData } = require("worker_threads");
+const captureframes = require("./worker");
+const { handleError } = require("./lib");
+(async function main() {
+  try {
+    if (isMainThread) {
+      const {
+        clgDonkeyLogo,
+        getProcessArguments,
+        prepareFramePartitions,
+        createFramesWithWorkers,
+        createVideo,
+      } = require("./lib");
 
-  const os = require("os");
-  const cpuCount = os.cpus().length;
+      clgDonkeyLogo();
 
-  const captureScreenshots = require("./worker");
+      // read process arguments
+      const { inputArguments, outputArguments } = await getProcessArguments();
 
-  if (isMainThread) {
-    const ffmpeg = require("ffmpeg-stream").ffmpeg;
-    const puppeteer = require("puppeteer");
-    const streamifier = require("streamifier");
-    const path = require("path");
-    const cliProgress = require("cli-progress");
+      // prepare milliseconds for frames in particions based on number of threads
+      const { framePartitions, totalFrames } = prepareFramePartitions(
+        inputArguments
+      );
 
-    console.log(`
-  _____                    _                      _____   _   _         
- |  __ \\                  | |                    / ____| | | (_)        
- | |  | |   ___    _ __   | | __   ___   _   _  | |      | |  _   _ __  
- | |  | |  / _ \\  | '_ \\  | |/ /  / _ \\ | | | | | |      | | | | | '_ \\ 
- | |__| | | (_) | | | | | |   <  |  __/ | |_| | | |____  | | | | | |_) |
- |_____/   \\___/  |_| |_| |_|\\_\\  \\___|  \\__, |  \\_____| |_| |_| | .__/ 
-                                          __/ |                  | |    
-                                         |___/                   |_|    `);
-    //Get Process Argument (GPA)
-    const gpa = (arg) =>
-      process.argv.indexOf(arg) + 1 &&
-      process.argv[process.argv.indexOf(arg) + 1];
-    const handleError = (e) => {
-      progressFrames.stop();
-      console.error(e.message + "\n");
-      process.exit();
-    };
-    const progressFrames = new cliProgress.SingleBar(
-      {},
-      cliProgress.Presets.shades_classic
-    );
-    const progressFfmpeg = new cliProgress.SingleBar(
-      {},
-      cliProgress.Presets.shades_classic
-    );
+      // capture frame screenshots
+      const frames = await createFramesWithWorkers(
+        framePartitions,
+        totalFrames,
+        inputArguments
+      );
 
-    (async () => {
-      const id = gpa("-i");
-      const output = gpa("-o") || "out.mp4";
-      const frameRate = Number(gpa("-r")) || 25;
-      const quality = Number(gpa("-q")) || 100;
-      const width = Number(gpa("-w")) || 1920; // set default to clip container params and a fallback
-      const height = Number(gpa("-h")) || 1080;
-
-      const msStart = Number(gpa("-s")) || 0;
-      let msEnd = Number(gpa("-e")) || 0;
-
-      const threads = Number(gpa("-c")) || cpuCount / 2;
-      const env = gpa("-env") || "production";
-      let url;
-      if (env == "production") {
-        url = "https://api.donkeyclip.com/embed/";
-      } else if (env == "staging") {
-        url = "https://staging-api.donkeyclip.com/embed/";
-      } else if (env == "local") {
-        url = "http://localhost:3030/embed/";
-      }
-      try {
-        console.log(" Initializing...\n");
-        const browser = await puppeteer.launch({
-          args: ["--no-sandbox", "--disable-setuid-sandbox"],
-          headless: true,
-        });
-        const page = await browser.newPage({ waitUntil: "networkidle" });
-        const htmlContent = `
-        <body>
-        <style>
-          html,body{width:100%;height:100%;padding: 0;margin: 0;}
-          #host{position: absolute;width:100%;height: 100%;top:0px;left: 0px}
-        </style>
-          <div id="host">
-            <script data-scale-to-fit src="${url}${id}/"></script>
-          </div>
-        </body>
-     `;
-        await page.setContent(htmlContent);
-
-        if (!msEnd) {
-          msEnd = await page.evaluate(`window.mc.Player.clip.duration`);
-        }
-
-        const step = 1000 / frameRate;
-        //create the files
-
-        const millisecondsPartitions = [];
-        const partLength = Math.floor((msEnd - msStart) / step / threads);
-        let part = 0;
-        let totalScreeshots = 0;
-        for (let ms = msStart; ms <= msEnd; ms += step) {
-          totalScreeshots++;
-          millisecondsPartitions[part] = millisecondsPartitions[part] || [];
-          millisecondsPartitions[part].push(ms);
-
-          if (millisecondsPartitions[part].length >= partLength) {
-            part++;
-          }
-        }
-        console.log(" Capturing frames...\n");
-
-        progressFrames.start(totalScreeshots, 0);
-
-        const screeshotPromises = [];
-
-        for (let i = 0; i < threads; i++) {
-          screeshotPromises.push(
-            new Promise((resolve, reject) => {
-              const worker = new Worker(__filename, {
-                workerData: {
-                  url,
-                  id,
-                  millisecondsArray: millisecondsPartitions[i],
-                  width,
-                  height,
-                  quality,
-                },
-              });
-              worker.on("message", (message) => {
-                if (message.increment) {
-                  progressFrames.increment();
-                } else {
-                  resolve({ i, message });
-                }
-              });
-              worker.on("error", reject);
-              worker.on("exit", (code) => {
-                if (code !== 0)
-                  reject(new Error(`Worker stopped with exit code ${code}`));
-              });
-            })
-          );
-        }
-
-        const frames = await Promise.all(screeshotPromises).then((result) => {
-          progressFrames.stop();
-          return result.reduce((prev, next) => [...prev, ...next.message], []);
-        });
-
-        console.log(" Creating Video:");
-        progressFfmpeg.start(frames.length, 0);
-        const conv = ffmpeg(); // create converter
-        const input = conv.input({ f: "image2pipe", r: frameRate });
-        conv.output(path.join(process.cwd(), output), {
-          vcodec: "libx264",
-          pix_fmt: "yuv420p",
-        });
-        frames
-          .map((buffer) => () =>
-            new Promise((fulfill, reject) => {
-              streamifier
-                .createReadStream(buffer) //<--- here's the main difference
-                .on("end", fulfill)
-                .on("error", reject)
-                .pipe(input, { end: false });
-            })
-          )
-          .reduce((prev, next) => {
-            progressFfmpeg.increment();
-            return prev.then(next);
-          }, Promise.resolve())
-          .then(() => input.end())
-          .catch(handleError);
-        conv
-          .run()
-          .then(() => {
-            progressFrames.stop();
-            console.log(" Enjoy!\n");
-            return process.exit();
-          })
-          .catch(handleError);
-      } catch (e) {
-        handleError(e);
-      }
-    })();
-  } else {
-    const threadFrames = await captureScreenshots(workerData);
-    parentPort.postMessage(threadFrames);
+      // creating the mp4 video
+      createVideo(frames, outputArguments);
+    } else {
+      const workerFrames = await captureframes(workerData);
+      parentPort.postMessage(workerFrames);
+    }
+  } catch (e) {
+    handleError(e);
   }
 })();
